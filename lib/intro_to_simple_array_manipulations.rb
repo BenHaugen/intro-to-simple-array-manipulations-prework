f1d9a90ee19e56a766b25fe451689a32525f5cdd
Ben Haugen <ben1232jazz@hotmail.com>
2019-01-23 05:00:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -35,3 +35,14 @@ def using_uniq(array)
   array.uniq
 end 
 
+def using_flatten
+  
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, number)
+  
+end
